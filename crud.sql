f5fc7bd99e08011ca0361cdd6f2063a0412a1351
Marcuspvh <marcuspvh@gmail.com>
2022-10-03 16:48:50 -0400

    Entrega

--- crud.sql
+++ crud.sql
@@ -4,31 +4,90 @@
 -- Criação
 
 -- 1)
-
+INSERT INTO
+	clientes (nome, lealdade)
+VALUES
+    ('Georgia', 0);
 
 
 -- 2)
-
+INSERT INTO 
+	pedidos (status, cliente_id)
+VALUES
+    ('Recebido', 6);
 
 
 -- 3)
-
+INSERT INTO 
+	produtos_pedidos (pedido_id, produto_id)
+VALUES
+    (6, 1),
+	(6, 2),
+	(6, 6),
+	(6, 8),
+	(6, 8);
 
 
 -- Leitura
 
 -- 1)
-
+SELECT 
+	c.id,
+	c.nome,
+	c.lealdade,
+	p.id,
+	p.status,
+	p.cliente_id,
+	p2.id,
+	p2.nome,
+	p2.tipo,
+	p2.preço,
+	p2.pts_de_lealdade
+FROM
+	pedidos p 
+JOIN
+	produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN 
+	produtos p2 ON p2.id = pp.produto_id
+JOIN
+	clientes c ON c.id = p.cliente_id 
+WHERE
+	c.nome ILIKE 'georgia';
 
 
 -- Atualização
 
 -- 1)
+SELECT 
+	SUM(p2.pts_de_lealdade)  
+FROM
+	pedidos p 
+JOIN
+	produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN 
+	produtos p2 ON p2.id = pp.produto_id
+JOIN
+	clientes c ON c.id = p.cliente_id 
+WHERE
+	c.nome ILIKE 'georgia';
+
+-- 2)
+UPDATE 
+	clientes c 
+SET
+	lealdade = 48
+WHERE 
+	c.nome ILIKE 'georgia';
+
 
 
 
 -- Deleção
 
 -- 1)
-
+DELETE FROM
+	clientes C
+WHERE 
+	c.nome ILIKE 'marcelo'
+RETURNING *;
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,82 @@
 -- Seleções de dados
 
 -- 1)
+--1.Consulte por todos os pedidos juntamente com todas as informações dos produtos que estão em cada um dos pedidos.
+
+SELECT 
+	p.id,
+	p.status,
+	p.cliente_id,
+	p2.id,
+	p2.nome,
+	p2.tipo,
+	p2.preço,
+	p2.pts_de_lealdade
+FROM
+	pedidos p 
+JOIN
+	produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN 
+	produtos p2 ON p2.id = pp.produto_id;
 
 -- 2)
+--2.Consulte pelo ID de todos os pedidos que incluam 'Fritas'.
+
+SELECT 
+	p.id
+FROM
+	pedidos p 
+JOIN
+	produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN 
+	produtos p2 ON p2.id = pp.produto_id
+WHERE
+	p2.nome ILIKE 'fritas';
 
 -- 3)
+--3.Consulte novamente por pedidos que incluam 'Fritas', porém agora, retorne apenas uma coluna (gostam_de_fritas) com o nome dos Clientes que fizeram os pedidos.
+
+SELECT 
+	c.nome AS gostam_de_fritas
+FROM
+	pedidos p 
+JOIN
+	produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN 
+	produtos p2 ON p2.id = pp.produto_id
+JOIN
+	clientes c ON c.id = p.cliente_id 
+WHERE
+	p2.nome ILIKE 'fritas';
 
 -- 4)
+--4.Crie uma query que retorne o custo total dos pedidos da 'Laura'.
+
+SELECT 
+	SUM(p2.preço)  
+FROM
+	pedidos p 
+JOIN
+	produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN 
+	produtos p2 ON p2.id = pp.produto_id
+JOIN
+	clientes c ON c.id = p.cliente_id 
+WHERE
+	c.nome ILIKE 'laura';
 
 -- 5)
+--5.Crie uma query que retorne em uma coluna o nome do produto, e na outra, o número de vezes que ele foi pedido. Dica: a função built-in COUNT pode somar o número de ocorrências de um id em uma coluna.
+
+SELECT 
+	p2.nome,
+	count(pp.produto_id) AS contagem 
+FROM
+	pedidos p 
+JOIN
+	produtos_pedidos pp ON p.id = pp.pedido_id
+JOIN 
+	produtos p2 ON p2.id = pp.produto_id
+JOIN
+	clientes c ON c.id = p.cliente_id
+GROUP BY p2.nome;
